57338bde1b8a3fe7da8d53119616ca2393ac1a65
brian-akong <brian.akong@tempus.com>
2021-12-22 12:56:13 -0600

    Make directory names and file names consistent

--- make_directories_for_days.sh
+++ make_directories_for_days.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# loop from 3 to 25
-# mkdir day_{loop_number}
+# Make directories for each day
 
 max=25
 for i in `seq 1 $max`
 do
-    mkdir -p "day_$i"
+    mkdir -p "day-$i"
 done
+
+# Below command changes all day_# directory names with underscores to hyphens
+# find . -type d -name "day_*" | sed -e "p;s/_/-/g" | xargs -n2 mv
